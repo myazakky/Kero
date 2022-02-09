7ed486b92067f02933ea37a2c917d95ad96be768
myazakky <nimuc@outlook.com>
2022-02-09 15:21:22 +0900

    isbnから情報を取得できるようにした

--- main.scm
+++ main.scm
@@ -14,10 +14,16 @@
       :secure #t)
     (parse-json-string body)))
 
-(define title (sjsonpath "$.voklumeInfo.title"))
-(define image (sjsonpath "$.voklumeInfo.imageLinks.Large"))
-(define authors (sjsonpath "$.voklumeInfo.authors"))
-(define published-date (sjsonpath "$.volumeInfo.publishedDate"))
+(define title (sjsonpath "$.items[0].volumeInfo.title"))
+(define image (sjsonpath "$.items[0].volumeInfo.imageLinks.thumbnail"))
+(define authors (sjsonpath "$.items[0].volumeInfo.authors[0]"))
+(define published-date (sjsonpath "$.items[0].volumeInfo.publishedDate"))
+
+(define (get-by-isbn con isbn)
+  (dbi-do con #"SELECT * from book where (isbn = '~|isbn|');"))
+
+(define-http-handler "/"
+  (^[req app] (respond/ok req "<h1>It worked!</h1>")))
 
 ;; GET: /api/books/{isbn}/post
 (define-http-handler #/\/api\/books\/(\d+)\/post/
@@ -30,11 +36,29 @@
         (unwind-protect (begin
           (dbi-execute insert
             isbn
-            (title data)
-            (image data)
-            (authors data)
-            (published-date data)))
+            (car (title data))
+            (car (image data))
+            (car (authors data))
+            (car (published-date data))))
           (dbi-close con)))
       (respond/ok req "ok"))))
+
+(define-http-handler #/\/api\/books\/(\d+)\/get/
+  (^[req app]
+    (let-params req ([isbn "p:1"])
+      (let* ((con (dbi-connect "dbi:sqlite:shelf.sqlite"))
+              (result (get-by-isbn con isbn))
+              [getter (relation-accessor result)])
+        (unwind-protect (begin
+          (respond/ok req (map (^r
+            (format #f
+              "{isbn: ~s, title: ~s, image: ~s, authors: ~s, publishedDate: ~s}"
+              (getter r "isbn")
+              (getter r "title")
+              (getter r "image")
+              (getter r "authors")
+              (getter r "publishedDate")))
+            (relation-rows result))))
+          (dbi-close con))))))
 
 (define (main args) (start-http-server :port 6789 :error-log #t :access-log #t))
